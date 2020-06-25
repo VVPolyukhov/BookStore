db986f524bcb1feb573779291be5ee4088c90ae9
Polyukhov Vladislav <vlad.polyukhov@gmail.com>
2020-06-25 15:17:08 +0300

    Updates

---
+++ precache-manifest.21de82ddea7bf040cdb6ed8324fa076e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f1e2a3476a08cf5fa0b0f94b3dfcd4",
+    "url": "/BookStore/index.html"
+  },
+  {
+    "revision": "e862dca1dc47572db2c7",
+    "url": "/BookStore/static/css/main.441d1b13.chunk.css"
+  },
+  {
+    "revision": "52a3d2c7abef3f2ec1b5",
+    "url": "/BookStore/static/js/2.8e27ee01.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/BookStore/static/js/2.8e27ee01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e862dca1dc47572db2c7",
+    "url": "/BookStore/static/js/main.f266a176.chunk.js"
+  },
+  {
+    "revision": "17160849bd6fd260018a",
+    "url": "/BookStore/static/js/runtime-main.0f924139.js"
+  }
+]);
